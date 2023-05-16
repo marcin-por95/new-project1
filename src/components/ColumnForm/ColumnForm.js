886c3f1cc8ad18d16302374d144e539bbb9c8734
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-05-16 16:21:48 +0200

    create references to addCard from List to column

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,7 +1,8 @@
 import styles from './ColumnForm.module.scss';
-import { useState } from 'react';
+import {useState} from 'react';
 import Button from "../Button/Button";
 import TextInput from "../TextInput/TextInput";
+
 
 
 const ColumnForm = props => {
@@ -11,19 +12,20 @@ const ColumnForm = props => {
 
     const handleSubmit = e => {
         e.preventDefault();
-        props.action({ title: title, icon: icon });
+        props.action({title: title, icon: icon, columnId: props.columnId });
         setTitle('');
         setIcon('');
     };
 
     return (
         <form className={styles.columnForm} onSubmit={handleSubmit}>
-            <span>Title:</span> <TextInput value={title} onChange={e => setTitle(e.target.value)} />
-            <span>Icon:</span>  <TextInput value={icon} onChange={e => setIcon(e.target.value)} />
+            <span>Title:</span> <TextInput value={title} onChange={e => setTitle(e.target.value)}/>
+            <span>Icon:</span> <TextInput value={icon} onChange={e => setIcon(e.target.value)}/>
             <Button>Add column</Button>
         </form>
     );
 
 };
+
 
 export default ColumnForm;
