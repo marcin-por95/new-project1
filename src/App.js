7eb925cf0358f93b3a04beacff9ca88960fe2400
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-06-15 19:10:58 +0200

    new NavBar Component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import SearchForm from './components/SearchForm/SearchForm'
 import Hero from './components/Hero/Hero'
 import List from './components/List/List'
 import Container from "./components/Container/Container";
-
+import NavBar from './components/NavBar/NavBar';
 
 const App = () => {
-  return (
-      <Container>
-        <Hero/>
-        <SearchForm/>
-        <List/>
-      </Container>
-  );
+    return (
+        <main>
+            <NavBar/>
+            <Container>
+                <Hero/>
+                <SearchForm/>
+                <List/>
+            </Container>
+        </main>
+    );
 };
 
 export default App;
